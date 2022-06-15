c11b249bcd83a046dae81d100802f22ca16ff93a
Oussama Belkhadir <osamaalpha1997@gmail.com>
2022-06-15 17:22:34 +0100

    fixing all case

--- src/execute/cmd_exe.c
+++ src/execute/cmd_exe.c
@@ -6,31 +6,68 @@
 /*   By: obelkhad <obelkhad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/12 16:25:43 by obelkhad          #+#    #+#             */
-/*   Updated: 2022/06/15 15:03:17 by obelkhad         ###   ########.fr       */
+/*   Updated: 2022/06/15 16:42:16 by obelkhad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	cmd_execut(t_element *f_cmd, t_element *l_cmd, char **envp)
+void	joining(t_element *gathering, t_element *part)
 {
-	t_element *elm;
-	(void)envp;
-	(void)l_cmd;
-	(void)f_cmd;
-	t_token *token;
+	t_token		*token_gathering;
+	t_token		*token_part;
+	char		*holder;
 
+	token_gathering = (t_token *)gathering->content;
+	token_part = (t_token *)part->content;
+	holder = ft_strjoin(token_gathering->value, token_part->value);
+	free(token_gathering->value);
+	token_gathering->value = holder;
+}
+
+void	join_pieces(t_element *f_cmd, t_element *l_cmd)
+{
+	t_token		*token;
+	t_element	*elm;
+	t_element	*gathering;
+	int			join;
+
+	join = 1;
 	elm = f_cmd;
-
-	expanding(f_cmd, l_cmd, envp);
-
 	while (elm && elm->prev != l_cmd)
 	{
 		token = (t_token *)elm->content;
-        printf("[ %s ][%d]\n",token->value, token->type);
-        elm = elm->next;
+		if (token->to_join && join)
+		{
+			gathering = elm;
+			join = 0;
+		}
+		else if (token->to_join && !join)
+		{
+			joining(gathering, elm);
+			token->type = -1;
+		}
+		else
+			join = 1;
+		elm = elm->next;
 	}
-	printf("--------------------------\n");
+}
+
+void	cmd_execut(t_element *f_cmd, t_element *l_cmd, char **envp)
+{
+	t_element	*elm;
+	t_token		*tok;
+	elm = f_cmd;
+
+	expanding(f_cmd, l_cmd, envp);
+	join_pieces(f_cmd, l_cmd);
+	
+	while (elm &&elm->prev != l_cmd)
+	{
+		tok = (t_token *)elm->content;
+		printf("( %s )-( %d )-(%d)\n",tok->value, tok->type,tok->to_join);
+		elm = elm->next;
+	}
 
 	// pipe_parse
 	// fork_processes
